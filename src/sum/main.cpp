0f00dc80a7429da87945f5ccbbe17cccab6cd5d5
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-09-05 18:02:21 +0700

    Add src/sum/main.cpp (draft)

---
+++ src/sum/main.cpp
@@ -0,0 +1,26 @@
+#include <cxxopts.hpp>
+#include <fts.hpp>
+#include <iostream>
+
+int main(int argc, char **argv) {
+    cxxopts::Options options("sqrt");
+    try {
+        // clang-format off
+        options.add_options()
+          ("a", "Floating-point number", cxxopts::value<double>())
+          ("b", "Floating-point number", cxxopts::value<double>());
+        // clang-format on
+
+        const auto result = options.parse(argc, argv);
+        if (argc != 2) {
+            std::cout << options.help() << "\n";
+            return 0;
+        }
+        const auto a = result["a"].as<double>();
+        const auto b = result["b"].as<double>();
+        std::cout << fts::sum(a, b) << "\n";
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << "\n";
+        return 1;
+    }
+}
