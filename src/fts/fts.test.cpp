516130f67f43180f1f23a338dd2e0729e26d9178
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-09-07 21:30:45 +0700

    Format code of test

--- src/fts/fts.test.cpp
+++ src/fts/fts.test.cpp
@@ -2,11 +2,9 @@
 
 #include <gtest/gtest.h>
 
-TEST(SumTest, Trivial) {
-    EXPECT_EQ(3, fts::sum(1, 2));
-}
+TEST(SumTest, Trivial) { EXPECT_EQ(3, fts::sum(1, 2)); }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
