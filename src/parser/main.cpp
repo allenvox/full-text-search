991a3891c12c452bc17be503501d3b04092f76b4
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-09-11 13:36:35 +0700

    Remove static function call from main parser file

--- src/parser/main.cpp
+++ src/parser/main.cpp
@@ -12,13 +12,13 @@ std::string get_user_input(const std::string &message) {
 
 int main() {
     std::string text = get_user_input("Enter the text to be parsed:");
-    std::unordered_set<std::string> stop_words = NgramParser::split_stop_words(
+    NgramParser parser;
+    std::vector<std::string> stop_words = parser.split_in_words(
         get_user_input("Enter the stop words separated with spaces:"), ' ');
     size_t minlen = static_cast<size_t>(
         std::stoi(get_user_input("Enter minimum ngram length:")));
     size_t maxlen = static_cast<size_t>(
         std::stoi(get_user_input("Enter maximum ngram length:")));
-    NgramParser parser;
     for (const std::string &token :
          parser.parse(text, stop_words, minlen, maxlen)) {
         std::cout << token << ' ';
