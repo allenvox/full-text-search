abb70fe63463fbd4385f4f7b421531c6f804726e
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-09-11 13:34:36 +0700

    Add utility functions to NgramParser class in header file

--- include/common/common.hpp
+++ include/common/common.hpp
@@ -8,30 +8,30 @@ class Parser {
   public:
     virtual std::vector<std::string>
     parse(const std::string &text,
-          const std::unordered_set<std::string> &stop_words,
-          size_t min_ngram_length, size_t max_ngram_length) const = 0;
+          const std::vector<std::string> &stop_words,
+          size_t ngram_min_length, size_t ngram_max_length) const = 0;
 };
 
 class NgramParser : public Parser {
   public:
+    std::string clear_text(const std::string &source) const;
+
+    std::vector<std::string> split_in_words(const std::string &text,
+                                            const char separator) const;
+
+    std::vector<std::string>
+    remove_stop_words(const std::vector<std::string> &words,
+                      const std::vector<std::string> &stop_words) const;
+
+    std::vector<std::string>
+    generate_ngrams(const std::vector<std::string> &words,
+                    const std::vector<std::string> &stop_words,
+                    const size_t ngram_min_length,
+                    const size_t ngram_max_length) const;
+
     std::vector<std::string>
     parse(const std::string &text,
-          const std::unordered_set<std::string> &stop_words,
-          size_t min_ngram_length, size_t max_ngram_length) const override;
-
-    static std::unordered_set<std::string>
-    split_stop_words(const std::string &stop_words_str, const char separator) {
-        std::unordered_set<std::string> stop_words;
-        size_t start = 0;
-        size_t end = stop_words_str.find(separator);
-        while (end != std::string::npos) {
-            std::string word = stop_words_str.substr(start, end - start);
-            stop_words.insert(word);
-            start = end + 1;
-            end = stop_words_str.find(separator, start);
-        }
-        std::string last_word = stop_words_str.substr(start);
-        stop_words.insert(last_word);
-        return stop_words;
-    }
+          const std::vector<std::string> &stop_words,
+          const size_t ngram_min_length,
+          const size_t ngram_max_length) const override;
 };
