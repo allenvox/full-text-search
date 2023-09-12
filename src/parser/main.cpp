0791bf172dc42f7f6b563d511424aa5f21ddc0b6
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-09-12 20:43:54 +0700

    Add const expressions & improve readability with synonyms

--- src/parser/main.cpp
+++ src/parser/main.cpp
@@ -2,24 +2,24 @@
 
 #include <iostream>
 
-std::string get_user_input(const std::string &message) {
+NgramText get_user_input(const NgramText &message) {
     std::cout << message << "\n"
               << "> ";
-    std::string input;
+    NgramText input;
     getline(std::cin, input);
     return input;
 }
 
 int main() {
-    std::string text = get_user_input("Enter the text to be parsed:");
+    const NgramText text = get_user_input("Enter the text to be parsed:");
     NgramParser parser;
-    std::vector<std::string> stop_words = parser.split_in_words(
+    const NgramStopWords stop_words = parser.split_in_words(
         get_user_input("Enter the stop words separated with spaces:"), ' ');
-    size_t minlen = static_cast<size_t>(
+    const NgramLength minlen = static_cast<size_t>(
         std::stoi(get_user_input("Enter minimum ngram length:")));
-    size_t maxlen = static_cast<size_t>(
+    const NgramLength maxlen = static_cast<size_t>(
         std::stoi(get_user_input("Enter maximum ngram length:")));
-    for (const std::string &token :
+    for (const NgramWord &token :
          parser.parse(text, stop_words, minlen, maxlen)) {
         std::cout << token << ' ';
     }
