5e009949f66058ab4859ed80b7eb654fcf10268b
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-09-14 12:13:22 +0700

    Format code

--- src/parser/main.cpp
+++ src/parser/main.cpp
@@ -3,8 +3,7 @@
 #include <iostream>
 
 NgramText get_user_input(const NgramText &message) {
-    std::cout << message << '\n'
-              << "> ";
+    std::cout << message << '\n' << "> ";
     NgramText input;
     getline(std::cin, input);
     return input;
@@ -19,8 +18,7 @@ int main() {
         std::stoi(get_user_input("Enter minimum ngram length:")));
     const NgramLength maxlen = static_cast<size_t>(
         std::stoi(get_user_input("Enter maximum ngram length:")));
-    for (const Ngram &ngram :
-         parser.parse(text, stop_words, minlen, maxlen)) {
+    for (const Ngram &ngram : parser.parse(text, stop_words, minlen, maxlen)) {
         std::cout << ngram.text << ' ' << ngram.pos << ' ';
     }
     std::cout << std::endl;
