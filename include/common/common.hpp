85a730829ccf004138763513152738df9ac56cbe
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-09-14 08:39:35 +0700

    Add NgramPair alias, fix parse() prototype

--- include/common/common.hpp
+++ include/common/common.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 #include <string>
-#include <vector>
 #include <utility>
+#include <vector>
 
 using NgramStopWords = std::vector<std::string>;
 using NgramWords = std::vector<std::string>;
@@ -10,11 +10,12 @@ using NgramLength = std::size_t;
 using NgramIndex = std::size_t;
 using NgramText = std::string;
 using NgramWord = std::string;
-using NgramPairs = std::vector<std::pair<NgramWord, NgramIndex>>;
+using NgramPair = std::pair<NgramWord, NgramIndex>;
+using NgramPairs = std::vector<NgramPair>;
 
 class Parser {
   public:
-    virtual NgramWords parse(const NgramText &text,
+    virtual NgramPairs parse(const NgramText &text,
                              const NgramStopWords &stop_words,
                              NgramLength ngram_min_length,
                              NgramLength ngram_max_length) const = 0;
@@ -38,7 +39,7 @@ class NgramParser : public Parser {
                                const NgramLength ngram_min_length,
                                const NgramLength ngram_max_length) const;
 
-    NgramWords parse(const NgramText &text, const NgramStopWords &stop_words,
+    NgramPairs parse(const NgramText &text, const NgramStopWords &stop_words,
                      const NgramLength ngram_min_length,
                      const NgramLength ngram_max_length) const override;
 };
