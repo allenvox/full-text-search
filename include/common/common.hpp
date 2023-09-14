762d192f6abdd76febe9518025f6f0df4e59a538
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-09-14 08:46:47 +0700

    Implement Ngram struct instead of pair

--- include/common/common.hpp
+++ include/common/common.hpp
@@ -10,12 +10,16 @@ using NgramLength = std::size_t;
 using NgramIndex = std::size_t;
 using NgramText = std::string;
 using NgramWord = std::string;
-using NgramPair = std::pair<NgramWord, NgramIndex>;
-using NgramPairs = std::vector<NgramPair>;
+using NgramVec = std::vector<Ngram>;
+
+struct {
+  NgramWord text;
+  NgramIndex pos;
+} Ngram;
 
 class Parser {
   public:
-    virtual NgramPairs parse(const NgramText &text,
+    virtual NgramVec parse(const NgramText &text,
                              const NgramStopWords &stop_words,
                              NgramLength ngram_min_length,
                              NgramLength ngram_max_length) const = 0;
@@ -34,12 +38,12 @@ class NgramParser : public Parser {
     NgramWords remove_stop_words(const NgramWords &words,
                                  const NgramStopWords &stop_words) const;
 
-    NgramPairs generate_ngrams(const NgramWords &words,
+    NgramVec generate_ngrams(const NgramWords &words,
                                const NgramStopWords &stop_words,
                                const NgramLength ngram_min_length,
                                const NgramLength ngram_max_length) const;
 
-    NgramPairs parse(const NgramText &text, const NgramStopWords &stop_words,
+    NgramVec parse(const NgramText &text, const NgramStopWords &stop_words,
                      const NgramLength ngram_min_length,
                      const NgramLength ngram_max_length) const override;
 };
