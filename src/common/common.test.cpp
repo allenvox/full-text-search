b757a582604703b84752415d8c50dffcc44de1d2
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-09-14 07:29:23 +0700

    Implement new unit tests for NgramParser

--- src/common/common.test.cpp
+++ src/common/common.test.cpp
@@ -26,6 +26,14 @@ TEST(ClearTextTest, Combined) {
     EXPECT_EQ(expected, result);
 }
 
+TEST(ClearTextTest, EmptyInput) {
+    const NgramParser parser;
+    const NgramText input = "";
+    const NgramText result = parser.clear_text(input);
+    const NgramText expected = "";
+    EXPECT_EQ(expected, result);
+}
+
 TEST(SplitInWordsTest, Trivial) {
     const NgramParser parser;
     const NgramText input = "Dr. Jekyll and Mr. Hyde";
@@ -34,10 +42,27 @@ TEST(SplitInWordsTest, Trivial) {
     EXPECT_EQ(expected, result);
 }
 
+TEST(SplitInWordsTest, EmptyInput) {
+    const NgramParser parser;
+    const NgramText input = "";
+    const NgramWords result = parser.split_in_words(input);
+    const NgramWords expected{""};
+    EXPECT_EQ(expected, result);
+}
+
 TEST(RemoveStopWordsTest, Trivial) {
     const NgramParser parser;
     const NgramWords input_arg1{"dr", "jekyll", "and", "mr", "hyde"};
     const NgramStopWords input_arg2{"and", "dr", "mr"};
+    const NgramWords result = parser.remove_stop_words(input_arg1, input_arg2);
+    const NgramWords expected{"jekyll", "hyde"};
+    EXPECT_EQ(expected, result);
+}
+
+TEST(RemoveStopWordsTest, NoStopWords) {
+    const NgramParser parser;
+    const NgramWords input_arg1{"jekyll", "hyde"};
+    const NgramStopWords input_arg2{};
     const NgramWords result = parser.remove_stop_words(input_arg1, input_arg2);
     const NgramWords expected{"jekyll", "hyde"};
     EXPECT_EQ(expected, result);
@@ -54,6 +79,16 @@ TEST(GenerateNgramsTest, Trivial) {
     EXPECT_EQ(expected, result);
 }
 
+TEST(GenerateNgramsTest, NoWords) {
+    const NgramParser parser;
+    const NgramWords input{};
+    const NgramLength minlen = 3;
+    const NgramLength maxlen = 6;
+    const NgramWords result = parser.generate_ngrams(input, {}, minlen, maxlen);
+    const NgramWords expected;
+    EXPECT_EQ(expected, result);
+}
+
 TEST(ParseTest, Trivial) {
     const NgramParser parser;
     const NgramText input = "Dr. Jekyll and Mr. Hyde";
@@ -63,6 +98,17 @@ TEST(ParseTest, Trivial) {
     const NgramWords result = parser.parse(input, stop_words, minlen, maxlen);
     const NgramWords expected{"jek 0",    "jeky 0", "jekyl 0",
                               "jekyll 0", "hyd 1",  "hyde 1"};
+    EXPECT_EQ(expected, result);
+}
+
+TEST(ParseTest, NoWords) {
+    const NgramParser parser;
+    const NgramText input = "";
+    const NgramStopWords stop_words{"and", "dr", "mr"};
+    const NgramLength minlen = 3;
+    const NgramLength maxlen = 6;
+    const NgramWords result = parser.parse(input, stop_words, minlen, maxlen);
+    const NgramWords expected;
     EXPECT_EQ(expected, result);
 }
 
