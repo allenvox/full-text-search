19d183113f2a80bfded2e3335e2319e75359fdd8
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-09-29 01:02:12 +0700

    Implement config header file for parser config

---
+++ include/config/config.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <common/common.hpp>
+
+struct Config {
+    NgramStopWords stop_words;
+    NgramLength min_length;
+    NgramLength max_length;
+};
+using Config = struct Config;
