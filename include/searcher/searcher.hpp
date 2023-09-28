169456e4e085dfe6e39abf0b6212f7c306da9e81
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-09-29 01:09:01 +0700

    Implement searcher lib header

---
+++ include/searcher/searcher.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <config/config.hpp>
+#include <indexer/indexer.hpp>
+#include <vector>
+
+struct Result {
+    IndexID doc_id;
+    double score;
+};
+using Result = struct Result;
+using Results = std::vector<Result>;
+using TermInfos = std::string;
+using SearcherQuery = std::string;
+using DocsCount = std::size_t;
+
+class IndexAccessor {
+  public:
+    virtual Config config() const = 0;
+    virtual TermInfos get_term_infos(IndexTerm term) const = 0;
+    virtual IndexText load_document(IndexID doc_id) const = 0;
+    virtual DocsCount total_docs() const = 0;
+};
+
+class TextIndexAccessor : public IndexAccessor {
+  public:
+    Config config() const;
+    TermInfos get_term_infos(IndexTerm term) const;
+    IndexText load_document(IndexID doc_id) const;
+    DocsCount total_docs() const;
+  private:
+    IndexPath path_;
+};
+
+namespace searcher {
+
+Results search(SearcherQuery query, IndexAccessor *ia);
+
+} // namespace searcher
