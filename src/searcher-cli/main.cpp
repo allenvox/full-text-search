a97f88875b9176cdcb3ba2108234430a3c88676f
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-10-05 16:21:18 +0700

    Implement searcher-cli program file

---
+++ src/searcher-cli/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <searcher/searcher.hpp>
+
+std::string get_user_input(const std::string &message) {
+    std::cout << message << '\n' << "> ";
+    std::string input;
+    getline(std::cin, input);
+    return input;
+}
+
+int main() {
+    const std::string idx_path = get_user_input("Enter input index path:");
+    const std::filesystem::path path = idx_path;
+    const std::string query = get_user_input("Enter search query:");
+    const TextIndexAccessor indexAccessor({{"the"}, 3, 6}, path);
+    Results results = searcher::search(query, indexAccessor);
+    std::cout << "id\tscore\ttext\n";
+    for (auto &result : results) {
+        IndexID id = result.doc_id;
+        std::cout << id << '\t' << result.score << '\t'
+                  << indexAccessor.load_document(id) << '\n';
+    }
+    return 0;
+}
