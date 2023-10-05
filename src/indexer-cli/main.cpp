4224a5c3e4555a8fc50541081568facff329d8dc
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-10-05 17:12:20 +0700

    Modify the index documents

--- src/indexer-cli/main.cpp
+++ src/indexer-cli/main.cpp
@@ -10,13 +10,22 @@ IndexText get_user_input(const IndexText &message) {
 }
 
 int main() {
-    IndexBuilder indexbuilder({"the"}, 3, 6);
-    indexbuilder.add_document(199903, "The Matrix");
-    indexbuilder.add_document(200305, "The Matrix Reloaded");
-    indexbuilder.add_document(200311, "The Matrix Revolution");
+    IndexBuilder indexbuilder({"and", "the", "of", "on"}, 3, 6);
+    indexbuilder.add_document(199903,
+                              "Harry Potter and the Philosopher's Stone");
+    indexbuilder.add_document(199925, "Harry Potter and the Deathly Hallows");
+    indexbuilder.add_document(199937,
+                              "Harry Potter and the Chamber of Secrets");
+    indexbuilder.add_document(199951, "The Shining");
+    indexbuilder.add_document(199964, "Infinite Jest");
+    indexbuilder.add_document(200101, "The Clockwork Orange");
+    indexbuilder.add_document(200305, "All Quiet on the Western Front");
     const Index index = indexbuilder.index();
 
-    const std::string out_path = get_user_input("Enter output path for index:");
+    std::string out_path = get_user_input("Enter output path for index:");
+    if (out_path.empty()) {
+        out_path = "build";
+    }
     const std::filesystem::path path = out_path;
 
     const TextIndexWriter writer;
