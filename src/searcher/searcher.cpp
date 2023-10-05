f0d4f8a5719c0add4061f28da64d22ebb89d0941
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-10-05 17:12:56 +0700

    Implement the searcher library file

---
+++ src/searcher/searcher.cpp
@@ -0,0 +1,94 @@
+#include <algorithm>
+#include <cmath>
+#include <common/common.hpp>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <picosha2.h>
+#include <searcher/searcher.hpp>
+#include <string>
+
+constexpr auto IDX_DOESNT_CONTAIN = "!!";
+
+Config TextIndexAccessor::config() const { return config_; }
+
+TermInfos TextIndexAccessor::get_term_infos(const IndexTerm &term) const {
+    std::string hash = picosha2::hash256_hex_string(term).substr(0, 6);
+    std::filesystem::path fullPath = path_ / "index" / "entries" / hash;
+    if (!std::filesystem::exists(fullPath)) {
+        return IDX_DOESNT_CONTAIN;
+    }
+    std::ifstream termFile(fullPath);
+    if (!termFile.is_open()) {
+        throw std::runtime_error("Can't open term file");
+    }
+    TermInfos termInfos("");
+    std::string line;
+    while (std::getline(termFile, line)) {
+        termInfos += line;
+    }
+    return termInfos;
+}
+
+IndexText TextIndexAccessor::load_document(IndexID doc_id) const {
+    std::string doc_name = std::to_string(doc_id);
+    std::ifstream docFile(path_ / "index" / "docs" / doc_name);
+    if (!docFile.is_open()) {
+        throw std::runtime_error("Can't open document file " + doc_name);
+    }
+    IndexText docText("");
+    std::string line;
+    while (std::getline(docFile, line)) {
+        docText += line;
+    }
+    return docText;
+}
+
+DocsCount TextIndexAccessor::total_docs() const {
+    DocsCount c = 0;
+    for (const auto &entry :
+         std::filesystem::directory_iterator(path_ / "index" / "docs")) {
+        if (entry.path() != "") {
+            ++c;
+        }
+    }
+    return c;
+}
+
+bool compare_desc_by_score(const Result &a, const Result &b) {
+    return a.score > b.score;
+}
+
+Results searcher::search(const SearcherQuery &query,
+                         const TextIndexAccessor &ia) {
+    const NgramParser parser;
+    const Config cfg = ia.config();
+    const NgramVec parsed =
+        parser.parse(query, cfg.stop_words, cfg.min_length, cfg.max_length);
+    std::map<IndexID, double> scores;
+    for (const auto &ngram : parsed) {
+        TermInfos termInfos = ia.get_term_infos(ngram.text);
+        if (termInfos == IDX_DOESNT_CONTAIN) {
+            continue;
+        }
+        NgramWords words = parser.split_in_words(termInfos);
+        double doc_frequency = std::stod(words[1]);
+        for (std::size_t i = 2; i < words.size() - 1; ++i) {
+            auto doc_id = static_cast<std::size_t>(std::stoi(words[i]));
+            ++i;
+            double term_frequency = std::stod(words[i]);
+            i += static_cast<std::size_t>(term_frequency);
+            auto N = static_cast<double>(static_cast<int>(ia.total_docs()));
+            double idf = log(N) - log(doc_frequency);
+            double tf_idf = term_frequency * idf;
+            scores[doc_id] += tf_idf;
+        }
+    }
+    Results results;
+    for (auto &score : scores) {
+        results.push_back({score.first, score.second});
+    }
+    std::sort(results.begin(), results.end(), compare_desc_by_score);
+    return results;
+}
