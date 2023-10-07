9cf7e9442b700f77315fe07471e3558f163ff841
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-10-07 11:51:53 +0700

    Format code

--- src/fts/fts.test.cpp
+++ src/fts/fts.test.cpp
@@ -5,6 +5,6 @@
 TEST(SumTest, Trivial) { EXPECT_EQ(3, fts::sum(1, 2)); }
 
 int main(int argc, char *argv[]) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
--- src/searcher-cli/main.cpp
+++ src/searcher-cli/main.cpp
@@ -2,30 +2,30 @@
 #include <searcher/searcher.hpp>
 
 std::string get_user_input(const std::string &message) {
-    std::cout << message << '\n' << "> ";
-    std::string input;
-    getline(std::cin, input);
-    return input;
+  std::cout << message << '\n' << "> ";
+  std::string input;
+  getline(std::cin, input);
+  return input;
 }
 
 int main() {
-    std::cout.setf(std::ios::fixed);
-    std::cout.precision(4);
+  std::cout.setf(std::ios::fixed);
+  std::cout.precision(4);
 
-    std::string idx_path = get_user_input("Enter input index path:");
-    if (idx_path.empty()) {
-        idx_path = "build";
-    }
-    const std::filesystem::path path = idx_path;
-    const std::string query = get_user_input("Enter search query:");
-    const TextIndexAccessor indexAccessor({{"the", "and", "of", "on"}, 3, 6},
-                                          path);
-    Results results = searcher::search(query, indexAccessor);
-    std::cout << "id\tscore\ttext\n";
-    for (auto &result : results) {
-        IndexID id = result.doc_id;
-        std::cout << id << '\t' << result.score << '\t'
-                  << indexAccessor.load_document(id) << '\n';
-    }
-    return 0;
+  std::string idx_path = get_user_input("Enter input index path:");
+  if (idx_path.empty()) {
+    idx_path = "build";
+  }
+  const std::filesystem::path path = idx_path;
+  const std::string query = get_user_input("Enter search query:");
+  const TextIndexAccessor indexAccessor({{"the", "and", "of", "on"}, 3, 6},
+                                        path);
+  Results results = searcher::search(query, indexAccessor);
+  std::cout << "id\tscore\ttext\n";
+  for (auto &result : results) {
+    IndexID id = result.doc_id;
+    std::cout << id << '\t' << result.score << '\t'
+              << indexAccessor.load_document(id) << '\n';
+  }
+  return 0;
 }
