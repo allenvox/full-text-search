d615317679771bbc01d22374f762dca51b0f657e
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-10-07 11:51:15 +0700

    Implement unit tests for searcher lib

--- src/searcher/searcher.test.cpp
+++ src/searcher/searcher.test.cpp
@@ -1,12 +1,53 @@
-#include <indexer/indexer.hpp>
-
+#include <fstream>
 #include <gtest/gtest.h>
+#include <searcher/searcher.hpp>
 
-TEST(SearcherTest, Sample) {
-    EXPECT_EQ(1, 1);
+IndexPath testPath = "build/test";
+IndexID testID = 363;
+IndexTerm testTerm = "hi";
+TermInfos testTermInfos = "hi 1 363 1 0 ";
+Config testConfig = {{}, 1, 3};
+DocsCount testDocsCount = 2;
+SearcherQuery testSearcherQuery = "hi";
+
+TEST(SearcherTest, GetConfig) {
+  const TextIndexAccessor indexAccessor(testConfig, testPath);
+  Config cfg = indexAccessor.config();
+  EXPECT_EQ(cfg.min_length, testConfig.min_length);
+  EXPECT_EQ(cfg.max_length, testConfig.max_length);
+}
+
+TEST(SearcherTest, GetTermInfos) {
+  IndexBuilder indexBuilder(testConfig.stop_words, testConfig.min_length,
+                            testConfig.max_length);
+  indexBuilder.add_document(testID, testTerm);
+  indexBuilder.add_document(testID + 1, "hello");
+  const Index index = indexBuilder.index();
+  const TextIndexWriter writer;
+  writer.write(testPath, index);
+  const TextIndexAccessor indexAccessor(testConfig, testPath);
+  const TermInfos termInfos = indexAccessor.get_term_infos(testTerm);
+  EXPECT_EQ(testTermInfos, termInfos);
+}
+
+TEST(SearcherTest, LoadDocument) {
+  const TextIndexAccessor indexAccessor(testConfig, testPath);
+  const IndexText docText = indexAccessor.load_document(testID);
+  EXPECT_EQ(docText, testTerm);
+}
+
+TEST(SearcherTest, TotalDocs) {
+  const TextIndexAccessor indexAccessor(testConfig, testPath);
+  EXPECT_EQ(indexAccessor.total_docs(), testDocsCount);
+}
+
+TEST(SearcherTest, Search) {
+  const TextIndexAccessor indexAccessor(testConfig, testPath);
+  Results results = searcher::search(testSearcherQuery, indexAccessor);
+  EXPECT_EQ(results[0].doc_id, testID);
 }
 
 int main(int argc, char *argv[]) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
