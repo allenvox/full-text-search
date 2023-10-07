399303dffbf066a9521a0e36812b2b639966bd29
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-10-07 12:09:17 +0700

    Format common lib header

--- include/common/common.hpp
+++ include/common/common.hpp
@@ -10,39 +10,39 @@ using NgramIndex = std::size_t;
 using NgramText = std::string;
 using NgramWord = std::string;
 struct Ngram {
-    std::string text;
-    std::size_t pos;
+  std::string text;
+  std::size_t pos;
 };
 using Ngram = struct Ngram;
 using NgramVec = std::vector<Ngram>;
 
 class Parser {
-  public:
-    virtual NgramVec parse(const NgramText &text,
-                           const NgramStopWords &stop_words,
-                           NgramLength ngram_min_length,
-                           NgramLength ngram_max_length) const = 0;
+public:
+  virtual NgramVec parse(const NgramText &text,
+                         const NgramStopWords &stop_words,
+                         NgramLength ngram_min_length,
+                         NgramLength ngram_max_length) const = 0;
 };
 
 class NgramParser : public Parser {
-  public:
-    bool is_stop_word(const NgramWord &word,
-                      const NgramStopWords &stop_words) const;
+public:
+  bool is_stop_word(const NgramWord &word,
+                    const NgramStopWords &stop_words) const;
 
-    NgramText clear_text(const NgramText &source) const;
+  NgramText clear_text(const NgramText &source) const;
 
-    NgramWords split_in_words(const NgramText &text,
-                              const char separator = ' ') const;
+  NgramWords split_in_words(const NgramText &text,
+                            const char separator = ' ') const;
 
-    NgramWords remove_stop_words(const NgramWords &words,
-                                 const NgramStopWords &stop_words) const;
+  NgramWords remove_stop_words(const NgramWords &words,
+                               const NgramStopWords &stop_words) const;
 
-    NgramVec generate_ngrams(const NgramWords &words,
-                             const NgramStopWords &stop_words,
-                             const NgramLength ngram_min_length,
-                             const NgramLength ngram_max_length) const;
+  NgramVec generate_ngrams(const NgramWords &words,
+                           const NgramStopWords &stop_words,
+                           const NgramLength ngram_min_length,
+                           const NgramLength ngram_max_length) const;
 
-    NgramVec parse(const NgramText &text, const NgramStopWords &stop_words,
-                   const NgramLength ngram_min_length,
-                   const NgramLength ngram_max_length) const override;
+  NgramVec parse(const NgramText &text, const NgramStopWords &stop_words,
+                 const NgramLength ngram_min_length,
+                 const NgramLength ngram_max_length) const override;
 };
