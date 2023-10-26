d030a1305ae80db93420947ee578ec8144dbabba
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-10-26 11:05:25 +0700

    Implement driver lib functions

---
+++ src/driver/driver.cpp
@@ -0,0 +1,44 @@
+#include <rapidcsv.h>
+
+#include <driver/driver.hpp>
+#include <indexer/indexer.hpp>
+
+#include <iostream>
+#include <unordered_map>
+
+namespace driver {
+
+void check_if_exists(const IndexPath& path) {
+  if (!std::filesystem::exists(path)) {
+    std::string err = "error: " + static_cast<std::string>(path) + " doesn't exist\n";
+    throw std::runtime_error(err);
+  }
+}
+
+rapidcsv::Document get_csv(const IndexPath& path) {
+  rapidcsv::Document doc(path);
+  return doc;
+}
+
+void remove_unneeded_cols(rapidcsv::Document& csv) {
+  for(std::size_t i = 2; i < csv.GetColumnCount(); ++i) {
+    csv.RemoveColumn(i);
+  }
+}
+
+Index generate_index(rapidcsv::Document& csv, Config& cfg) {
+  IndexBuilder indexBuilder(cfg);
+  remove_unneeded_cols(csv);
+  for(std::size_t i = 1; i < csv.GetRowCount(); ++i) {
+    IndexID id = csv.GetCell<IndexID>(0, i);
+    IndexText txt = csv.GetCell<IndexText>(1, i);
+    indexBuilder.add_document(id, txt);
+  }
+  return indexBuilder.index();
+}
+
+void write_index(const IndexPath &path, const Index &idx, const IndexWriter &iw) {
+  iw.write(path, idx);
+}
+
+} // namespace driver
