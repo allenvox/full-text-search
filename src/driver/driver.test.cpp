486f62a18d780632559986c88c922e78e0717401
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-11-01 20:38:57 +0700

    Fix driver lib unit tests with temp csv document testDoc()

--- src/driver/driver.test.cpp
+++ src/driver/driver.test.cpp
@@ -2,27 +2,48 @@
 
 #include <driver/driver.hpp>
 
-std::filesystem::path testPath = "build/test.csv";
-std::filesystem::path fakePath = "fake";
+#include <iostream>
+
+IndexPath testPath = "test.csv";
+IndexPath fakePath = "fake.csv";
+
+rapidcsv::Document testDoc() {
+  rapidcsv::Document doc;
+  doc.InsertColumn(0, std::vector<std::size_t>{101, 202, 303}, "id");
+  doc.InsertColumn(
+      1, std::vector<std::string>{"Welcome", "Hello", "Good morning"}, "name");
+  doc.InsertColumn(2, std::vector<int>{-1, 3, 1}, "random");
+  doc.RemoveColumn(3);
+  return doc;
+}
 
 TEST(DriverTest, CheckExist) {
+  rapidcsv::Document doc = testDoc();
+  doc.Save(testPath);
   EXPECT_THROW(driver::check_if_exists(fakePath), std::runtime_error);
   EXPECT_NO_THROW(driver::check_if_exists(testPath));
 }
 
 TEST(DriverTest, GetCSV) {
-  rapidcsv::Document doc = driver::get_csv(testPath);
+  rapidcsv::Document test = testDoc();
+  test.Save(testPath);
+  const rapidcsv::Document doc = driver::get_csv(testPath);
   EXPECT_EQ(doc.GetColumnCount(), 3);
 }
 
 TEST(DriverTest, RemoveCols) {
-  rapidcsv::Document doc = driver::get_csv(testPath);
+  rapidcsv::Document doc = testDoc();
   driver::remove_unneeded_cols(doc);
   EXPECT_EQ(doc.GetColumnCount(), 2);
 }
 
 TEST(DriverTest, GenerateIdx) {
-  Index index = driver::generate_index(testPath);
+  rapidcsv::Document doc = testDoc();
+  doc.Save(testPath);
+  const Index index = driver::generate_index(testPath);
+  for (auto [id, text] : index.docs) {
+    std::cout << id << ' ' << text << '\n';
+  }
   EXPECT_EQ(index.docs.size(), 3);
 }
 
