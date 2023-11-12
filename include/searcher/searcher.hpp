33485c2636c03008dcc1f5ccbfd1ad66387cda98
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-11-12 17:42:18 +0700

    Remove indexer 'using' synonyms

--- include/searcher/searcher.hpp
+++ include/searcher/searcher.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
 #include <config/config.hpp>
-#include <indexer/indexer.hpp>
+
+#include <filesystem>
 #include <vector>
 
 struct Result {
-  IndexID doc_id;
+  std::size_t doc_id;
   double score;
 };
 using Result = struct Result;
@@ -17,23 +18,24 @@ using DocsCount = std::size_t;
 class IndexAccessor {
 public:
   virtual Config config() const = 0;
-  virtual TermInfos get_term_infos(const IndexTerm &term) const = 0;
-  virtual IndexText load_document(IndexID doc_id) const = 0;
+  virtual TermInfos get_term_infos(const std::string &term) const = 0;
+  virtual std::string load_document(std::size_t doc_id) const = 0;
   virtual DocsCount total_docs() const = 0;
 };
 
 class TextIndexAccessor : public IndexAccessor {
 public:
-  TextIndexAccessor(const IndexPath &path, Config &config = DEFAULT_CONFIG)
+  TextIndexAccessor(const std::filesystem::path &path,
+                    Config &config = DEFAULT_CONFIG)
       : config_(std::move(config)), path_(std::move(path)){};
   Config config() const;
-  TermInfos get_term_infos(const IndexTerm &term) const;
-  IndexText load_document(IndexID doc_id) const;
+  TermInfos get_term_infos(const std::string &term) const;
+  std::string load_document(std::size_t doc_id) const;
   DocsCount total_docs() const;
 
 private:
   Config config_;
-  IndexPath path_;
+  std::filesystem::path path_;
 };
 
 namespace searcher {
