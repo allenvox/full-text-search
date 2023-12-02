873e2bdbf31a11964bf06be753d027c7312f257b
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-12-02 21:29:16 +0700

    Implement binary Accessors' constructors

--- include/searcher/searcher.hpp
+++ include/searcher/searcher.hpp
@@ -3,6 +3,7 @@
 #include <config/config.hpp>
 
 #include <filesystem>
+#include <sys/mman.h>
 #include <vector>
 
 struct Result {
@@ -25,21 +26,73 @@ public:
 
 class TextIndexAccessor : public IndexAccessor {
 public:
-  TextIndexAccessor(const std::filesystem::path &path,
-                    Config &config = DEFAULT_CONFIG)
+  explicit TextIndexAccessor(std::filesystem::path &path,
+                             Config &config = DEFAULT_CONFIG)
       : config_(std::move(config)), path_(std::move(path)){};
-  Config config() const;
-  TermInfos get_term_infos(const std::string &term) const;
-  std::string load_document(std::size_t doc_id) const;
-  DocsCount total_docs() const;
+  Config config() const override;
+  TermInfos get_term_infos(const std::string &term) const override;
+  std::string load_document(std::size_t doc_id) const override;
+  DocsCount total_docs() const override;
 
 private:
   Config config_;
   std::filesystem::path path_;
 };
 
+class DictionaryAccessor {
+public:
+  explicit DictionaryAccessor(uint32_t begin, uint32_t end, std::vector<uint8_t> data)
+      : data_(data.begin() + begin, data.begin() + end){};
+  uint32_t retrieve(std::string word) const;
+private:
+  std::vector<uint8_t> data_;
+};
+
+class EntriesAccessor {
+public:
+  explicit EntriesAccessor(uint32_t begin, uint32_t end, std::vector<uint8_t> data)
+      : data_(data.begin() + begin, data.begin() + end){};
+  TermInfos get_term_infos(uint32_t offset) const;
+private:
+  std::vector<uint8_t> data_;
+};
+
+class DocumentsAccessor {
+public:
+  explicit DocumentsAccessor(uint32_t begin, uint32_t end, std::vector<uint8_t> data)
+      : data_(data.begin() + begin, data.begin() + end){};
+  std::string load_document(uint32_t id) const;
+  uint32_t total_docs() const;
+private:
+  std::vector<uint8_t> data_;
+};
+
+class BinaryIndexAccessor : public IndexAccessor {
+public:
+  explicit BinaryIndexAccessor(std::filesystem::path &path);
+  void print_data() const;
+  Config config() const override;
+  TermInfos get_term_infos(const std::string &term) const override {
+    return ea_->get_term_infos(term.size());
+  };
+  std::string load_document(std::size_t doc_id) const override {
+      return doa_->load_document(doc_id);
+  };
+  DocsCount total_docs() const override {
+      return doa_->total_docs();
+  };
+
+private:
+  uint32_t get_u32_from_u8s(uint32_t start, const std::vector<uint8_t> &bytes);
+  std::vector<uint8_t> data_;
+  std::size_t fsize_;
+  DictionaryAccessor *dia_;
+  EntriesAccessor *ea_;
+  DocumentsAccessor *doa_;
+};
+
 namespace searcher {
 
-Results search(const SearcherQuery &query, const TextIndexAccessor &ia);
+Results search(const SearcherQuery &query, const IndexAccessor &ia);
 
 } // namespace searcher
