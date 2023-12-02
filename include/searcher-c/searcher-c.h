ce87e580c8b2fc71283aad1d93cb3f8d52bb50bb
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-12-02 21:30:16 +0700

    Add different initializers & finalizers to searcher-c lib (text/binary searchers)

--- include/searcher-c/searcher-c.h
+++ include/searcher-c/searcher-c.h
@@ -4,9 +4,11 @@
 extern "C" {
 #endif
 
-void *initialize_searcher(const char *index_path);
-void finalize_searcher(void *accessor);
-void search(void *accessor, const char *query, unsigned int *doc_ids,
+void *initialize_text_searcher(const char *index_path);
+void *initialize_binary_searcher(const char *index_path);
+void finalize_text_searcher(void *accessor);
+void finalize_binary_searcher(void *accessor);
+void text_search(void *accessor, const char *query, unsigned int *doc_ids,
             double *scores, unsigned int *result_count);
 
 #ifdef __cplusplus
