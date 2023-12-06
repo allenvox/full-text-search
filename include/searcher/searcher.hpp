a7766df4f1d120bc6fa05b3b246e427fb04d40db
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-12-06 12:36:07 +0700

    Redo searcher header accessors' classes structure

--- include/searcher/searcher.hpp
+++ include/searcher/searcher.hpp
@@ -1,27 +1,25 @@
 #pragma once
 
+#include <binary/binary.hpp>
 #include <config/config.hpp>
 
 #include <filesystem>
 #include <sys/mman.h>
+#include <unordered_map>
 #include <vector>
 
 struct Result {
-  std::size_t doc_id;
+  size_t doc_id;
   double score;
 };
-using Result = struct Result;
 using Results = std::vector<Result>;
-using TermInfos = std::string;
-using SearcherQuery = std::string;
-using DocsCount = std::size_t;
 
 class IndexAccessor {
 public:
   virtual Config config() const = 0;
-  virtual TermInfos get_term_infos(const std::string &term) const = 0;
-  virtual std::string load_document(std::size_t doc_id) const = 0;
-  virtual DocsCount total_docs() const = 0;
+  virtual std::string get_term_infos(const std::string &term) const = 0;
+  virtual std::string load_document(size_t doc_id) const = 0;
+  virtual size_t total_docs() const = 0;
 };
 
 class TextIndexAccessor : public IndexAccessor {
@@ -30,71 +28,94 @@ public:
                              Config &config = DEFAULT_CONFIG)
       : config_(std::move(config)), path_(std::move(path)){};
   Config config() const override { return config_; };
-  TermInfos get_term_infos(const std::string &term) const override;
-  std::string load_document(std::size_t doc_id) const override;
-  DocsCount total_docs() const override;
+  std::string get_term_infos(const std::string &term) const override;
+  std::string load_document(size_t doc_id) const override;
+  size_t total_docs() const override;
 
 private:
   Config config_;
   std::filesystem::path path_;
 };
 
+class DocumentsAccessor {
+public:
+  explicit DocumentsAccessor(
+      BinaryHandler &data,
+      std::unordered_map<std::string, size_t> &section_offsets)
+      : data_(data), section_offsets_(&section_offsets) {
+    data_.set_current_position(section_offsets_->at("docs"));
+    data_.read(total_docs_);
+  }
+  size_t total_docs() const { return total_docs_; }
+  std::string load_document(size_t offset);
+
+private:
+  BinaryHandler data_;
+  std::unordered_map<std::string, size_t> *section_offsets_;
+  uint32_t total_docs_{0};
+};
+
 class DictionaryAccessor {
 public:
-  explicit DictionaryAccessor(uint32_t begin, uint32_t end, std::vector<uint8_t> data)
-      : data_(data.begin() + begin, data.begin() + end){};
-  uint32_t retrieve(const std::string &word) const;
+  explicit DictionaryAccessor(
+      BinaryHandler &data,
+      std::unordered_map<std::string, size_t> &section_offsets)
+      : data_(data), section_offsets_(&section_offsets) {}
+  size_t retrieve(const std::string &word);
+
 private:
-  std::vector<uint8_t> data_;
+  BinaryHandler data_;
+  std::unordered_map<std::string, size_t> *section_offsets_;
 };
 
 class EntriesAccessor {
 public:
-  explicit EntriesAccessor(uint32_t begin, uint32_t end, std::vector<uint8_t> data)
-      : data_(data.begin() + begin, data.begin() + end){};
-  TermInfos get_term_infos(const std::string &term, const DictionaryAccessor *dia) const;
-private:
-  std::vector<uint8_t> data_;
-};
+  explicit EntriesAccessor(
+      BinaryHandler &data,
+      std::unordered_map<std::string, size_t> &section_offsets)
+      : data_(data), section_offsets_(&section_offsets) {
+    dia_ = new DictionaryAccessor(data_, section_offsets);
+  }
+  std::string get_term_infos(const std::string &term);
 
-class DocumentsAccessor {
-public:
-  explicit DocumentsAccessor(uint32_t begin, uint32_t end, std::vector<uint8_t> data)
-      : data_(data.begin() + begin, data.begin() + end){};
-  std::string load_document(uint32_t offset) const;
-  uint32_t total_docs() const;
 private:
-  std::vector<uint8_t> data_;
+  BinaryHandler data_;
+  std::unordered_map<std::string, size_t> *section_offsets_;
+  DictionaryAccessor *dia_;
 };
 
 class BinaryIndexAccessor : public IndexAccessor {
 public:
-  explicit BinaryIndexAccessor(std::filesystem::path &path,
+  explicit BinaryIndexAccessor(const std::filesystem::path &path,
                                Config &config = DEFAULT_CONFIG);
-  void print_data() const;
-  Config config() const override { return config_; };
-  TermInfos get_term_infos(const std::string &term) const override {
-    return ea_->get_term_infos(term, dia_);
-  };
-  std::string load_document(std::size_t offset) const override {
-      return doa_->load_document(offset);
-  };
-  DocsCount total_docs() const override {
-      return doa_->total_docs();
-  };
+  BinaryIndexAccessor(BinaryIndexAccessor &other) = default;
+  BinaryIndexAccessor(BinaryIndexAccessor &&other) = default;
+  ~BinaryIndexAccessor();
+  BinaryIndexAccessor &operator=(const BinaryIndexAccessor &other) = default;
+  BinaryIndexAccessor &operator=(BinaryIndexAccessor &&other) = default;
+  Config config() const override { return config_; }
+
+  size_t total_docs() const override { return doa_->total_docs(); }
+
+  std::string load_document(size_t id) const override {
+    return doa_->load_document(id);
+  }
+
+  std::string get_term_infos(const std::string &term) const override {
+    return ea_->get_term_infos(term);
+  }
 
 private:
-  uint32_t get_u32_from_u8s(uint32_t start, const std::vector<uint8_t> &bytes);
-  Config config_;
-  std::vector<uint8_t> data_;
-  std::size_t fsize_;
-  DictionaryAccessor *dia_;
-  EntriesAccessor *ea_;
+  int fd_;
+  BinaryHandler data_;
+  std::unordered_map<std::string, size_t> section_offsets_;
   DocumentsAccessor *doa_;
+  EntriesAccessor *ea_;
+  Config config_;
 };
 
 namespace searcher {
 
-Results search(const SearcherQuery &query, const IndexAccessor &ia);
+Results search(const std::string &query, const IndexAccessor &ia);
 
 } // namespace searcher
